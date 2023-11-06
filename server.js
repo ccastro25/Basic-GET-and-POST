3bf41f3ac8ac1a1a2d1510f7b1405620a5a68ec3
ccastro25 <caonabocastro25@gmail.com>
2023-11-06 00:31:08 -0500

    removed puppeteer and a few other unnessary dependencies. Also using fetch to get the db results to the frontend

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const puppeteer = require('puppeteer');
+
 const queryMysql = require('./mysqlconnection');
 const express = require('express');
 const domResults = require('./getDomContents')
 const app = express();
-const bodyParser = require('body-parser');
 
 // Create application/x-www-form-urlencoded parser
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -16,47 +15,17 @@ app.get('/index.html', function(req,res){
 })
 
 
-
- 
-
 app.get('/process_get', function (req, res) {
     // Prepare output in JSON format
     
     response = queryMysql();
  
-   
     queryMysql();
     res.end(response);
     domResults();
-    //pup()
+   
  })
 
-
-
-//this reponds a Delete rquest for the del_user page
-app.delete('del_user',function(req, res){
-    console.log("goot a delete rquest for /del_user");
-    res.send('Page listing');
-})
-
-
-
-async function pup() {
-    const browser = await puppeteer.launch();
-    page = await browser.newPage();
-        await page.goto('file:///Users/caonabocastro/Desktop/basicRouting/BasicGetAndPost/index.html', {waitUntil: 'load'});
-
-
-    const newPage = await page.evaluate(() => {
-
-        return  document.getElementById("result").innerText;
-
-        });
-
-     console.log(newPage)
-  
-    await browser.close();
-  };
 
 let server = app.listen(8081, function () {
    let host = server.address().address

8c4c62fd5304f76bc24f93d74f8002453502c777
ccastro25 <caonabocastro25@gmail.com>
2023-11-13 23:45:37 -0500

    combined selenium and beautifulSoup, currently parsing through source page

--- testingSelenium.py
+++ testingSelenium.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 
 browser = webdriver.Safari()
 browser.get("https://www.walmart.com/search?q=Milk&affinityOverride=store_led&facet=fulfillment_method%3APickup")
 browser.page_source
-#print(browser.title)
+soup = BeautifulSoup(browser.page_source)
+print(soup.prettify())

2c17cf7a2b0ed7394078609453de459e1d0b7af3
ccastro25 <caonabocastro25@gmail.com>
2023-11-14 01:49:57 -0500

    combined selenium and beautiful. I can now get the price and description of product. next step is to insert data to the walmart table

--- testingSelenium.py
+++ testingSelenium.py
@@ -5,6 +5,14 @@ from bs4 import BeautifulSoup
 
 browser = webdriver.Safari()
 browser.get("https://www.walmart.com/search?q=Milk&affinityOverride=store_led&facet=fulfillment_method%3APickup")
-browser.page_source
+
 soup = BeautifulSoup(browser.page_source)
-print(soup.prettify())
+prods = soup.find_all("div",class_='mb0')
+
+for i in range(len(prods)):
+    span = prods[0].find_all('span')
+    price = span[8].text.split("price ")[1]
+    title = span[0].text
+    #call db and insert values 
+
+

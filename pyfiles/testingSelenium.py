92302f89bd62a5a7c7ee7e49390246d85093dc37
ccastro25 <caonabocastro25@gmail.com>
2023-11-16 00:23:01 -0500

    changed products from dict to list

--- pyfiles/testingSelenium.py
+++ pyfiles/testingSelenium.py
@@ -12,21 +12,22 @@ browser.get("https://www.walmart.com/search?q=Milk&affinityOverride=store_led&fa
 soup = BeautifulSoup(browser.page_source)
 prods = soup.find_all("div",class_='mb0')
 
-products = {}
-
+products = []
+span_count=0
 for p_count , value in  enumerate(prods):
     span = value.find_all('span',class_="w_iUH7")
-    if len(span)>1: 
-     products[p_count] =[today]    
-     for count , value in  enumerate(span):
+    if len(span)>1:
+      products.append([today]) 
+      p_len = len(products)-1
+      for count , value in  enumerate(span):
         if count <=1:
             if count==1:
-              products[p_count].insert(count,value.text.split("price $")[1])
+              products[p_len].insert(count,value.text.split("price $")[1])
             else:
-                 products[p_count].insert(count,value.text)
+                 products[p_len].insert(count,value.text)
 
 seq_of_params =[]
-for key, value in  products.items:
-    seq_of_params.append(tuple(value))
-
-insert_data(seq_of_params):
+for items in  products:
+    seq_of_params.append(tuple(items))
+print(seq_of_params)
+#insert_data(seq_of_params)

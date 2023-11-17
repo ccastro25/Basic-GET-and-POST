bbda65d7a884d24e2b0e1420015174e697514063
ccastro25 <caonabocastro25@gmail.com>
2023-11-16 23:35:17 -0500

    added grocery list. list contains all the products to scrape.I also  changed it to a single loop, which does the same as previous.

--- pyfiles/scrape_products.py
+++ pyfiles/scrape_products.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+rom selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
@@ -7,27 +7,66 @@ from datetime import  datetime
    
 today = datetime.now().date()
 browser = webdriver.Safari()
-browser.get("https://www.walmart.com/search?q=Milk&affinityOverride=store_led&facet=fulfillment_method%3APickup")
 
+grocery_list =[
+                'Eggs',
+                'Milk',
+                'Bread',
+                'Bacon',
+                'Cooking+oil',
+                'Rice',
+                'Tuna',
+                'Steak',
+                'Chicken', 
+                'Ham', 
+                'Cheese',
+                'Yogurt', 
+                'Platano',
+                'Frozen+pizza'
+                'Grapes',
+                'Strawberry',
+                'Blueberry', 
+                'Raspberry',
+                'Lettuce', 
+                'Tomatoes',
+                'Onion',
+                'Avocado',
+                'Cereals',
+                'Ice+cream',
+                'Cream+cheese', 
+                'Tomato+sauce',
+                'Spaghetti', 
+                'Lasagna+noodles', 
+                'Chocolate', 
+              ]
+def get_products(item):
+browser.get("https://www.walmart.com/search?q=milk&affinityOverride=store_led&facet=fulfillment_method%3APickup")#.format(item
 soup = BeautifulSoup(browser.page_source)
-prods = soup.find_all("div",class_='mb0')
-
+#just use soup.find_all('span',class_='w_iUH7') , just get spans. but will have to refactor loops 
+prods = soup.find_all('span',class_="w_iUH7")
+print(item)
 products = []
 span_count=0
 for p_count , value in  enumerate(prods):
-    span = value.find_all('span',class_="w_iUH7")
-    if len(span)>1:
-      products.append([today]) 
-      p_len = len(products)-1
-      for count , value in  enumerate(span):
-        if count <=1:
-            if count==1:
-              products[p_len].insert(count,value.text.split("price $")[1])
-            else:
-                 products[p_len].insert(count,value.text)
+    if p_count >1 and not 'reviews' in value.text:
+      print(value.text) 
+      if 'price' in value.text:
+        products[span_count].insert(1,value.text.split("price $")[1])
+        span_count +=1
+      else:
+        print(span_count)
+        products.append([today])
+        products[span_count].insert(0,value.text)
 
-seq_of_params =[]
+              
+print( products)
+
+final_list = []
+for item in grocery_list:
+    final_list.extend( get_products(item))
+
+list_of_tupples =[]
 for items in  products:
-    seq_of_params.append(tuple(items))
-print(seq_of_params)
-insert_data(seq_of_params)
+    list_of_tupples.append(tuple(items))
+
+insert_data(list_of_tupples)

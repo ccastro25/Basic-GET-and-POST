8bf8aeeda13f93e4db5434f00e06dd82698199aa
ccastro25 <caonabocastro25@gmail.com>
2023-11-21 00:10:32 -0500

    remove unnecessary whitespace

--- pyfiles/insert_data_to_mysql.py
+++ pyfiles/insert_data_to_mysql.py
@@ -2,16 +2,12 @@ import mysql.connector
 #table creation  CREATE TABLE walmartproducts (id INT not null auto_increment, product_name VARCHAR(30),price DECIMAL(5,2), the_date DATE, primary key(id));
 
 def insert_data(seq_of_params):
+  
     cnx = mysql.connector.connect(user='castro', password ='jnfh(*89LJd267*&ldkj',database='comparableproductsdb')
     cursor = cnx.cursor()
-
     add_products =  "INSERT INTO walmartproducts(product_name, price, the_date) VALUES(%s,%s,%s)"
-    
-   
     cursor.executemany(add_products,seq_of_params)
-
     cnx.commit()
-
     cursor.close()
     cnx.close()
 

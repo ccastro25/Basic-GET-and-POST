ae6e2cd834c2abee7766b196783f37b0df153bff
ccastro25 <caonabocastro25@gmail.com>
2023-11-22 00:05:32 -0500

    added store name to insert and table is now a parameter

--- pyfiles/insert_data_to_mysql.py
+++ pyfiles/insert_data_to_mysql.py
@@ -1,11 +1,12 @@
 import mysql.connector
-#table creation  CREATE TABLE walmartproducts (id INT not null auto_increment, product_name VARCHAR(30),price DECIMAL(5,2), the_date DATE, primary key(id));
+#table creation  CREATE TABLE walmartproducts (id INT not null auto_increment, product_name VARCHAR(100),price DECIMAL(5,2), the_date DATE,store_name VARCHAR(30), primary key(id));
 
-def insert_data(seq_of_params):
+def insert_data(seq_of_params,table):
   
     cnx = mysql.connector.connect(user='castro', password ='jnfh(*89LJd267*&ldkj',database='comparableproductsdb')
     cursor = cnx.cursor()
-    add_products =  "INSERT INTO walmartproducts(product_name, price, the_date) VALUES(%s,%s,%s)"
+    add_products =  "INSERT INTO"+table+"(product_name, price, the_date, store) VALUES(%s,%s,%s,%s)"
+ 
     cursor.executemany(add_products,seq_of_params)
     cnx.commit()
     cursor.close()

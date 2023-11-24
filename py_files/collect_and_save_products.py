5616dc8d8c2d9cec00bab56bb0683a7bd1ad462d
ccastro25 <caonabocastro25@gmail.com>
2023-11-23 19:44:45 -0500

    seperated the logic that iterates through grocery list and save to db and create new script with it

---
+++ py_files/collect_and_save_products.py
@@ -0,0 +1,29 @@
+from insert_data_to_mysql import insert_data
+from grocery_list import grocery_list
+import pickle
+import time
+
+def get_save_items(store_product,get_product):
+
+    products = []
+    for item in grocery_list:
+        print("current item: {0}".format(item))
+        products.extend( get_product(item))
+        print("starting")
+        time.sleep(20)
+        print("waiting 1 ") 
+        time.sleep(20)
+        print("waiting 2")
+        time.sleep(20)
+        print("waiting 3")
+        time.sleep(20)
+        print("done")
+    
+
+    with open(f'{store_product}.pickle','wb') as f:
+        pickle.dump(final_list,f) 
+    insert_data(final_list,store_product)
+
+    
+   
+ 

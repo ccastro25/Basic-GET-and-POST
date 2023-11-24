feb226f998310813be090ffc989916a9a1008b8b
ccastro25 <caonabocastro25@gmail.com>
2023-11-23 19:45:36 -0500

    remove section that saves and iterates through grocery list

--- py_files/scrape_acme_products.py
+++ py_files/scrape_acme_products.py
@@ -1,52 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import time
 from datetime import  datetime
-import pickle
+from bs4 import BeautifulSoup
+from collect_and_save_products import get_save_items
+import time 
 
 
-grocery_list =[ 'Raspberry',
-                'Egg',
-                'Milk',
-                'Bread',
-                'Bacon',
-                'Cooking+oil',
-                'Rice',
-                'Tuna',
-                'Steak',
-                'Chicken', 
-                'Ham', 
-                'Cheese',
-                'Yogurt', 
-                'Banana',
-                'Frozen+pizza',
-                'Grapes',
-                'Strawberry',
-                'Blueberry', 
-               
-                'Lettuce', 
-                'Tomatoes',
-                'Onion',
-                'Avocado',
-                'Cereals',
-                'Ice+cream',
-                'Cream+cheese', 
-                'Tomato+sauce',
-                'Spaghetti', 
-                'Lasagna+noodles', 
-                'Chocolate', 
-              ]
 today = datetime.now().date()
 
-def get_products(item):
+def get_acme_products(item):
     driver = webdriver.Safari()
 
     driver.get("https://www.acmemarkets.com/shop/search-results.html?q={0}".format(item))
     print("this is the url: "+"https://www.acmemarkets.com/shop/search-results.html?q={0}".format(item) )
     time.sleep(2)
-    soup = BeautifulSoup(driver.page_source)
+    soup = BeautifulSoup(driver.page_source,'html.parser')
     title = soup.find_all('a',attrs={'data-qa': 'prd-itm-pttl'})
     #price retrieves all values  
     price = soup.find_all('span',attrs={'data-qa':'prd-itm-prc'})
@@ -62,21 +31,4 @@ def get_products(item):
     print(products)
     return products
 
-final_list = []
-for item in grocery_list:
-    print("current item: {0}".format(item))
-    final_list.extend( get_products(item))
-    print("starting")
-    time.sleep(20)
-    print("waiting 1 ") 
-    time.sleep(20)
-    print("waiting 2")
-    time.sleep(20)
-    print("waiting 3")
-    time.sleep(20)
-    print("done")
-#save as pickle incase of error
-with open('acme_products.pickle','wb') as f:
-     pickle.dump(final_list,f)
-
-#insert_data(final_list,"acmeproducts")
+get_save_items('acme_products',get_acme_products)

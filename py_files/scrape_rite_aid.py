759161f557e24d42f3dc30fafad2500fd6321408
ccastro25 <caonabocastro25@gmail.com>
2023-11-26 22:54:11 -0500

    changed when driver is being called. driver called inside method and like includes filter

--- py_files/scrape_rite_aid.py
+++ py_files/scrape_rite_aid.py
@@ -8,38 +8,29 @@ from grocery_list import grocery_list
 import time 
 
 
-options = webdriver.SafariOptions()
+options = webdriver.ChromeOptions()
 #options.add_argument('--headless')
 options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-driver = webdriver.Safari(options=options)
-driver.get(f"https://www.riteaid.com/")
+driver = webdriver.Chrome(options=options)
+
 
 today = datetime.now().date()
 
 def get_acme_products(item):
-       
-time.sleep(2)
-inpu = driver.find_element(By.CLASS_NAME, "cx-header-menu__search-textbox")
+    driver.get(f"https://www.riteaid.com/shop/catalogsearch/result/?q={item}&Category=Food")    
+    time.sleep(2)
+                                
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    title =soup.find_all('div',class_='ra_prod_name')
+    price  =soup.find_all('span',class_='ra_final-price')
 
-inpu.clear()
-inpu.send_keys(item)
-time.sleep(2)
-inpu.send_keys(Keys.RETURN)
-time.sleep(5)
-inpu = driver.find_element(By.CLASS_NAME,"myCheckbox_43617465676f727920466f6f64")
-inpu.click()
-soup = BeautifulSoup(driver.page_source,'html.parser')
-title =soup.find_all('div',class_='ra_prod_name')
-price  =soup.find_all('span',class_='ra_final-price')
+    products =[]
+    for i,v  in enumerate(title):
+        products.append((title[i].text.strip(),price[i].text.replace('$', ''), today,"Rite-Aid"))
 
-
-products =[]
-for i,v  in enumerate(title):
-    products.append((title[i].text,price[i].text.split(' ')[2].replace('$', ''), today,"ACME"))
-
-print('this is a sample')
-print(products)
-return products
+    print('this is a sample')
+    print(products)
+    return products
 
 
 products = []

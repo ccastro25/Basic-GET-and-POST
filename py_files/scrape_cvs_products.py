8d1499fffcd16139498c86eb3351144b3dff156c
ccastro25 <caonabocastro25@gmail.com>
2023-12-03 23:09:56 -0500

    imported pickle and change driver to safari

--- py_files/scrape_cvs_products.py
+++ py_files/scrape_cvs_products.py
@@ -4,12 +4,13 @@ from selenium.webdriver.common.keys import Keys
 from datetime import  datetime
 from bs4 import BeautifulSoup
 import time
-import re 
+import re
+import pickle  
 #class="css-901oao css-cens5h r-b0vftf r-1xaesmv r-ubezar r-majxgm r-29m4ib r-rjixqe r-1mnahxq r-fdjqy7 r-13qz1uu"
-options = webdriver.ChromeOptions()
+options = webdriver.SafariOptions()
 options.add_argument('--headless')
 options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Safari(options=options)
 
 grocery_list =[ 'Raspberry',
                 'Egg',
@@ -45,7 +46,7 @@ grocery_list =[ 'Raspberry',
 today = datetime.now().date()
 
 def get_cvs_products(item):
-    driver.get(f"https://www.cvs.com/search?searchTerm=milk")
+    driver.get(f"https://www.cvs.com/search?searchTerm={item}")
     time.sleep(2)
 
     soup = BeautifulSoup(driver.page_source,'html.parser')
@@ -77,7 +78,7 @@ for item in grocery_list:
     print("done")
 driver.quit()
 
-with open('acme.pickle','wb') as f:
-    pickle.dump(final_list,f) 
-insert_data(final_list,store_product)
+with open('cvs.pickle','wb') as f:
+    pickle.dump(products,f) 
+#insert_data(final_list,store_product)
 

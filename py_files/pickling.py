931aa02c4975826e783024ec26541562218a504d
ccastro25 <caonabocastro25@gmail.com>
2023-12-07 22:34:10 -0500

    putting pickle in a single file

---
+++ py_files/pickling.py
@@ -0,0 +1,17 @@
+import pickle
+from insert_data_to_mysql import insert_data
+
+files = ['acmeproducts','cvsproducts','riteaidproducts','shopriteproducts','walmartproducts']
+
+def pickle_dumps(filename):
+    with open(filename,'wb') as f:
+        pickle.dump(products,f)
+
+def load_pickled_to_db():
+    for file in files:
+        print(file)
+        with open(f'../pickles/{file}.pickle','rb') as f:
+            data = pickle.load(f)
+            insert_data(data,file)
+
+load_pickled_to_db()

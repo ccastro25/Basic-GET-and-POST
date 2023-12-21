8e77c62d7c8372d1f684a8df7d257d676ae94b0b
ccastro25 <caonabocastro25@gmail.com>
2023-12-21 01:02:20 -0500

    pooling connectins  instead of crating and managing one by one

--- mysqlconnection.js
+++ mysqlconnection.js
@@ -1,14 +1,24 @@
 const mysql = require('mysql2/promise');
+stores = [ 'walmartproducts', 'shopriteproducts', 'cvsproducts', 'riteaidproducts', 'acmeproducts'] 
+
 module.exports = async function getSqlRows(searchTerm) {
+    const connection = await mysql.createPool({host:'localhost', user: 'castro',
+    password:'jnfh(*89LJd267*&ldkj',database:'comparableproductsdb'});
   
-    // create the connection
-    const connection = await mysql.createConnection({host:'localhost', user: 'castro',
-                                                    password:'jnfh(*89LJd267*&ldkj',database:'comparableproductsdb'});
-    // query database
-    let queryStatement = "select * from walmartproducts where product_name like '%"+searchTerm+"%'"
-    const [rows, fields] = await connection.execute(queryStatement);
-                                                   
+    try {
+        let allRows = [];
+
+        for (let i = 0; i < stores.length; i++) {
+          const tableName = stores[i];
+          const queryStatement = `SELECT * FROM ${tableName} WHERE product_name LIKE ?`;
+          const [rows, fields] = await connection.execute(queryStatement, [`%${searchTerm}%`]);
+          allRows.push({ tableName, rows });
+        }
     
-    connection.end();
-    return rows
-    }
+        return allRows;
+      } finally {
+        connection.release(); // Always release the connection, even if an error occurs
+      }
+  }
+
+    

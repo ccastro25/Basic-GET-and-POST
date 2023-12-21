a435ae3bf8db3fde57a0869fdbceef3001e7e3a7
ccastro25 <caonabocastro25@gmail.com>
2023-12-21 01:03:47 -0500

    allowing both front-end and back-end to communicate with cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const queryMysql = require('./mysqlconnection');
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -8,7 +8,15 @@ const bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-
+// Configure CORS to allow only requests from http://127.0.0.1:5500
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500',
+    optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+  };
+  
+  app.use(cors(corsOptions));
+  
+  // Your routes and other middleware go here
 app.get('/process_get/', async function (req, res) {
     // Prepare output in JSON format
      response =  await queryMysql(req.query.serachTerm);
